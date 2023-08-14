7c9ad3dd1c5317ff62fabd2ae2fd91cdb336a6fb
lewiscornelius <lewiscornelius7@gmail.com>
2023-08-14 15:55:38 -0400

    debugged function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
+
+function selectTopic() {
+ if (randomTopic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (randomTopic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (randomTopic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
 
 /*
 if (topic === 'HTML') {
@@ -17,3 +39,8 @@ if (topic === 'HTML') {
  console.log('Please try again!');
 }
 */
+
+<ul>
+   <li>A variable is a named container that allows us to store data in our code.</li>
+   <li>Control flow is the order in which a computer executes code in a script.</li>
+ </ul>
